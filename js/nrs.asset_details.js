bcb4651c3164f44a438a75d92a8bc2679e2defbe
TeStDrUiF <testdruif@testdruif.be>
2015-09-30 12:56:51 +0200

    First commit

---
+++ js/nrs.asset_details.js
@@ -0,0 +1,20 @@
+var NRS = (function(NRS, $, undefined) {
+	NRS.pages.p_asset_details = function() {
+		var rows = "";
+		
+		NRS.sendRequest("getAccountAssets", {account: NRS.accountRS}, function(response) {
+				$.each(response.accountAssets, function(i, assetinfo) {
+					rows += "<tr>";
+					rows += "<td>" + String(assetinfo.name).escapeHTML() + "</td>";
+					rows += "<td>" + String(assetinfo.quantityQNT).escapeHTML() + "</td>";
+					rows += "<td><a href=https://www.mynxt.info/asset/" +  String(assetinfo.asset).escapeHTML() + " target = _blank>" + String(assetinfo.asset).escapeHTML() + "</a></td>";
+					rows += "</tr>";
+				});
+			NRS.dataLoaded(rows);
+		});
+	}
+	NRS.setup.p_asset_details = function() {
+		//Do one-time initialization stuff here
+	}
+	return NRS;
+}(NRS || {}, jQuery));
