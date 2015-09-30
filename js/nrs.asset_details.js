ea428910cf4e47173854b75f1e09b1b12874da04
TeStDrUiF <testdruif@testdruif.be>
2015-09-30 18:53:47 +0200

    fixed decimals

--- js/nrs.asset_details.js
+++ js/nrs.asset_details.js
@@ -2,12 +2,13 @@ var NRS = (function(NRS, $, undefined) {
 
 	NRS.pages.p_asset_details = function() {
 		var rows = "";
-		
+		var quantity = 0;
 		NRS.sendRequest("getAccountAssets", {account: NRS.accountRS}, function(response) {
 				$.each(response.accountAssets, function(i, assetinfo) {
+					quantity = assetinfo.quantityQNT / Math.pow(10, assetinfo.decimals);
 					rows += "<tr>";
 					rows += "<td>" + String(assetinfo.name).escapeHTML() + "</td>";
-					rows += "<td>" + String(assetinfo.quantityQNT).escapeHTML() + "</td>";
+					rows += "<td>" + String(quantity).escapeHTML() + "</td>";
 					rows += "<td><a href=https://www.mynxt.info/asset/" +  String(assetinfo.asset).escapeHTML() + " target = _blank>" + String(assetinfo.asset).escapeHTML() + "</a></td>";
 					rows += "</tr>";
 				});
