cde07e1f2709bc1a7daa8059f462f96e54c3ecf4
TeStDrUiF <testdruif@testdruif.be>
2015-09-30 20:23:31 +0200

    account info

--- js/nrs.asset_details.js
+++ js/nrs.asset_details.js
@@ -1,23 +1,40 @@
 var NRS = (function(NRS, $, undefined) {
 
-	NRS.pages.p_asset_details = function() {
-		var rows = "";
-		var quantity = 0;
-		NRS.sendRequest("getAccountAssets", {account: NRS.accountRS}, function(response) {
-				$.each(response.accountAssets, function(i, assetinfo) {
-					quantity = assetinfo.quantityQNT / Math.pow(10, assetinfo.decimals);
-					rows += "<tr>";
-					rows += "<td>" + String(assetinfo.name).escapeHTML() + "</td>";
-					rows += "<td>" + String(quantity).escapeHTML() + "</td>";
-					rows += "<td><a href=https://www.mynxt.info/asset/" +  String(assetinfo.asset).escapeHTML() + " target = _blank>" + String(assetinfo.asset).escapeHTML() + "</a></td>";
-					rows += "</tr>";
-				});
-			NRS.dataLoaded(rows);
+AssetAmount = function() {
+	NRS.sendRequest("getAccountAssetCount", {account: NRS.accountRS}, function(AssetAmount) {
+		$("#asset_count").text(AssetAmount.numberOfAssets);
+	});
+}
+
+AssetAccount = function() {
+	$("#ass_acc").text(NRS.accountRS);
+}
+
+AssetInfo = function() {
+	var rows = "";
+	var quantity = 0;
+	NRS.sendRequest("getAccountAssets", {account: NRS.accountRS}, function(response) {
+		$.each(response.accountAssets, function(asset, assetinfo) {
+			quantity = assetinfo.quantityQNT / Math.pow(10, assetinfo.decimals);
+			rows += "<tr>";
+			rows += "<td>" + String(assetinfo.name).escapeHTML() + "</td>";
+			rows += "<td>" + String(quantity).escapeHTML() + "</td>";
+			rows += "<td><a href=https://www.mynxt.info/asset/" +  String(assetinfo.asset).escapeHTML() + " target = _blank>" + String(assetinfo.asset).escapeHTML() + "</a></td>";
+			rows += "</tr>";
 		});
-	}
-	NRS.setup.p_asset_details = function() {
-		//Do one-time initialization stuff here
-	}
-	return NRS;
+
+	NRS.dataLoaded(rows);
+
+	});
+}
+
+NRS.pages.p_asset_details = function() {
+	AssetAccount();
+	AssetAmount();
+	AssetInfo();
+}
+
+
+return NRS;
 }(NRS || {}, jQuery));
 
